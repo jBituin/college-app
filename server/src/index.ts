9bfa77e3949119c5bfe1b0b4357217c80c2dce1b
Joemel Bituin <bituinjoemel@gmail.com>
2021-01-27 00:43:02 +0800

    Set up database connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,18 @@ import mongoose from 'mongoose';
 const app = express();
 app.use(json());
 
+mongoose.connect(
+  'mongodb://localhost:27017/college',
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log('Connected to database');
+  }
+);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
