7f3af99b6ddc88ec8c86901ed1f8542ef9b6423d
Joemel Bituin <bituinjoemel@gmail.com>
2021-01-27 01:41:15 +0800

    Set up graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,41 @@
 import express from 'express';
+import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import cors from 'cors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
 
-const app = express();
-app.use(json());
+import { UserResolver } from './schema/resolvers/UserResolver';
 
-mongoose.connect(
-  'mongodb://localhost:27017/college',
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  () => {
-    console.log('Connected to database');
-  }
-);
+(async () => {
+  const app = express();
+  app.use(json());
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+  mongoose.connect(
+    'mongodb://localhost:27017/college',
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    () => {
+      console.log('Connected to database');
+    }
+  );
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver],
+    }),
+    context: ({ req, res }) => ({ req, res }),
+  });
+
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  const PORT = process.env.PORT || 3000;
+  app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+})();
