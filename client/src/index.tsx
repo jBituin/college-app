7e93373fab77d365b3290f2436c14d0892d67fae
Joemel Bituin <bituinjoemel@gmail.com>
2021-01-31 22:17:23 +0800

    Add reload persist handlers

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,39 +6,87 @@ import {
   ApolloClient,
   ApolloProvider,
   HttpLink,
+  from,
+  DefaultOptions,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import { getAccessToken } from './accessToken';
-import Routes from './Routes';
-
-const getToken = () => {
-  const token = getAccessToken();
-  console.log('token@getTokenAuth :', token);
-  return token ? `bearer ${token}` : '';
-};
+import { TokenRefreshLink } from 'apollo-link-token-refresh';
+import jwtDecode from 'jwt-decode';
+import { getAccessToken, setAccessToken } from './accessToken';
+import App from './App';
 
 const httpLink = new HttpLink({
   uri: 'http://localhost:4000/graphql',
   credentials: 'include',
 });
 
-const headerLink = setContext((_, previousContext) => ({
-  headers: {
-    ...previousContext.headers,
-    authorization: getToken(),
+const authLink = setContext((_, { headers }) => {
+  const token = getAccessToken();
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const tokenRefreshLink = new TokenRefreshLink({
+  accessTokenField: 'accessToken',
+  isTokenValidOrUndefined: () => {
+    const token = getAccessToken();
+    if (!token) return true;
+    try {
+      const decodedToken: any = jwtDecode(token);
+
+      if (Date.now() >= decodedToken.exp * 1000) {
+        return false;
+      } else {
+        return true;
+      }
+    } catch (e) {
+      console.log('Error here...');
+      return false;
+    }
   },
-}));
+  fetchAccessToken: () => {
+    return fetch('http://localhost:4000/refresh-token', {
+      method: 'POST',
+      credentials: 'include',
+    });
+  },
+  handleFetch: (accessToken) => {
+    console.log('accessToken :>> ', accessToken);
+    setAccessToken(accessToken);
+  },
+  handleError: (err) => {
+    console.warn('Your refresh token is invalid. Try to relogin');
+    console.log(err);
+  },
+});
+
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+};
 
 const client = new ApolloClient({
-  link: headerLink.concat(httpLink),
+  link: from([tokenRefreshLink, authLink, httpLink]),
   cache: new InMemoryCache(),
   credentials: 'include',
+  defaultOptions,
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <ChakraProvider>
-      <Routes />
+      <App />
     </ChakraProvider>
   </ApolloProvider>,
   document.getElementById('root')
