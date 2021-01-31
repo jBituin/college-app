fbffba4f955ed690f76c61382ee4667c81b77a47
Joemel Bituin <bituinjoemel@gmail.com>
2021-02-01 03:30:40 +0800

    Add generic listing page

---
+++ client/src/components/Listing.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
+
+interface Props {
+  data: any[];
+  dataKeys: any[];
+  headers: string[];
+  onRowClick?: () => {};
+}
+
+function resolve(path: string, obj: {}, separator = '.') {
+  const properties: any[] = Array.isArray(path) ? path : path.split(separator);
+  return properties.reduce(
+    (prev: any, curr: string) => prev && prev[curr],
+    obj
+  );
+}
+
+const Listing: React.FC<Props> = ({ data, dataKeys, headers, onRowClick }) => {
+  return (
+    <Table variant="simple" m={8}>
+      <Thead>
+        <Tr>
+          {headers.map((header) => (
+            <Th key={header}>{header}</Th>
+          ))}
+        </Tr>
+      </Thead>
+      <Tbody>
+        {data.map((item) => {
+          return (
+            <Tr key={item._id} onClick={onRowClick}>
+              {dataKeys.map((dataKey) => (
+                <Td key={resolve(dataKey, item)}>{resolve(dataKey, item)}</Td>
+              ))}
+            </Tr>
+          );
+        })}
+      </Tbody>
+    </Table>
+  );
+};
+
+export default Listing;
