13e035511d8a4e83bc8c10d27786c7f0c9fc37fa
Joemel Bituin <bituinjoemel@gmail.com>
2021-02-01 18:18:05 +0800

    Fix duplicate usernames

--- server/src/User/UserSchema.ts
+++ server/src/User/UserSchema.ts
@@ -1,8 +1,9 @@
-import { prop as Property } from '@typegoose/typegoose';
+import { prop as Property, index } from '@typegoose/typegoose';
 import { Field, ObjectType, ID } from 'type-graphql';
 import { ObjectId } from 'mongodb';
 
 @ObjectType()
+@index({ username: 1 }, { unique: true })
 export class User {
   @Field(() => ID)
   readonly _id: ObjectId;
